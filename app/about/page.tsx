81bc83c689d77f6a674e077da76ee244aedc757d
Gulshan-koondhar <153608541+Gulshan-koondhar@users.noreply.github.com>
2024-11-04 11:55:10 +0500

    font color change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <div className="max-w-screen-xl mx-auto p-4">
       <div>
         <h1 className="text-4xl font-bold mb-4">About Us</h1>
-        <p className="text-lg text-gray-600">
+        <p className="text-lg">
           Welcome to our blog! We are passionate about sharing insights,
           stories, and knowledge through words. Our aim is to bring value to
           readers by providing thoughtful articles on a variety of topics.
